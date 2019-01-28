a8a6c2c55c5f4a7f6e8e8290fbd9c9cce0b71950
renmay <renmay@qq.com>
2019-01-28 11:32:33 +0800

    firstCommit

---
+++ app/src/components/footer/footGuide.vue
@@ -0,0 +1,266 @@
+<template>
+  <section id='foot_guide'>
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+         style="position:absolute;width:0;height:0">
+      <defs>
+        <!--<symbol viewBox="0 0 40 40" id="msite">-->
+          <!--<g fill="none" fill-rule="evenodd" stroke="#666" stroke-width="2">-->
+            <!--<path-->
+              <!--d="M31.426 23.095l2.678 5.742 2.943-1.372a3.173 3.173 0 0 0 1.537-4.212l-1.339-2.871-5.819 2.713z"></path>-->
+            <!--<path-->
+              <!--d="M29.074 31.161c-1.224-.49-2.404-.32-3.49.185-6.383 2.977-13.938.286-16.875-6.01-2.936-6.297-.14-13.815 6.243-16.792 5.211-2.43 11.203-1.083 14.825 2.919l-12.263 5.718c-1.596.745-2.295 2.624-1.561 4.198.734 1.574 2.625 2.246 4.22 1.503l8.422-3.928 9.953-4.641a18.78 18.78 0 0 0-.941-2.453C33.202 2.416 21.869-1.62 12.294 2.844 2.718 7.309-1.474 18.586 2.93 28.03c4.404 9.445 15.737 13.482 25.313 9.017 1.069-.499 2.067-.879 3.438-1.744 0 0-1.382-3.651-2.607-4.142z"></path>-->
+          <!--</g>-->
+        <!--</symbol>-->
+        <!--<symbol xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 40 40" id="msiteActive">-->
+          <!--<defs>-->
+            <!--<linearGradient id="index.18edf5a_c" x1="50%" x2="50%" y1="100%" y2="0%">-->
+              <!--<stop offset="0%" stop-color="#2BAEFF"></stop>-->
+              <!--<stop offset="100%" stop-color="#0095FF"></stop>-->
+            <!--</linearGradient>-->
+            <!--<linearGradient id="index.18edf5a_d" x1="50%" x2="50%" y1="100%" y2="0%">-->
+              <!--<stop offset="0%" stop-color="#29ADFF"></stop>-->
+              <!--<stop offset="100%" stop-color="#0095FF"></stop>-->
+            <!--</linearGradient>-->
+            <!--<path id="index.18edf5a_a"-->
+                  <!--d="M30.426 22.095l2.678 5.742 2.943-1.372a3.173 3.173 0 0 0 1.537-4.212l-1.339-2.871-5.819 2.713z"></path>-->
+            <!--<mask id="index.18edf5a_e" width="9.455" height="10.456" x="-1" y="-1">-->
+              <!--<path fill="#fff" d="M29.426 18.382h9.455v10.456h-9.455z"></path>-->
+              <!--<use xlink:href="#index.18edf5a_a"></use>-->
+            <!--</mask>-->
+            <!--<path id="index.18edf5a_b"-->
+                  <!--d="M28.074 30.161c-1.224-.49-2.404-.32-3.49.185-6.383 2.977-13.938.286-16.875-6.01-2.936-6.297-.14-13.815 6.243-16.792 5.211-2.43 11.203-1.083 14.825 2.919l-12.263 5.718c-1.596.745-2.295 2.624-1.561 4.198.734 1.574 2.625 2.246 4.22 1.503l8.422-3.928 9.953-4.641a18.78 18.78 0 0 0-.941-2.453C32.202 1.416 20.869-2.62 11.294 1.844 1.718 6.309-2.474 17.586 1.93 27.03c4.404 9.445 15.737 13.482 25.313 9.017 1.069-.499 2.067-.879 3.438-1.744 0 0-1.382-3.651-2.607-4.142z"></path>-->
+            <!--<mask id="index.18edf5a_f" width="38.769" height="39.241" x="-.7" y="-.7">-->
+              <!--<path fill="#fff" d="M-.521-.675h38.769v39.241H-.521z"></path>-->
+              <!--<use xlink:href="#index.18edf5a_b"></use>-->
+            <!--</mask>-->
+          <!--</defs>-->
+          <!--<g fill="none" fill-rule="evenodd">-->
+            <!--<g transform="translate(1 1)">-->
+              <!--<use fill="url(#index.18edf5a_c)" xlink:href="#index.18edf5a_a"></use>-->
+              <!--<use stroke="url(#index.18edf5a_d)" stroke-width="2" mask="url(#index.18edf5a_e)"-->
+                   <!--xlink:href="#index.18edf5a_a"></use>-->
+            <!--</g>-->
+            <!--<g transform="translate(1 1)">-->
+              <!--<use fill="url(#index.18edf5a_c)" xlink:href="#index.18edf5a_b"></use>-->
+              <!--<use stroke="url(#index.18edf5a_d)" stroke-width="1.4" mask="url(#index.18edf5a_f)"-->
+                   <!--xlink:href="#index.18edf5a_b"></use>-->
+            <!--</g>-->
+          <!--</g>-->
+        <!--</symbol>-->
+        <symbol id="msite" viewBox="0 0 1030 1024">
+          <defs>
+          </defs>
+          <path
+            d="M771.074 1011.991h-60.113c-40.053 0-71.099-42.938-71.099-79.872V772.655c0-16.384-26.577-59.602-38.935-63.349l-163.165.14c-8.61.721-30.627 29.65-30.627 46.731V938.59c0 37.725-37.073 73.402-76.288 73.402h-71.726c-40.1 0-84.713-53.574-84.713-89.879V570.81c0-11.566-15.127-24.273-28.928-24.273l-70.982.838c-49.292 0-65.699-21.9-71.145-34.933-5.422-13.056-9.355-40.145 25.205-74.891l396.94-399.058C448.443 15.407 479.093 2.7 511.745 2.7h.023c32.675 0 63.325 12.707 86.296 35.793l396.94 399.034c34.56 34.77 30.626 61.86 25.204 74.915-5.423 13.056-21.853 34.933-70.89 34.933-12.869 0-23.272-10.427-23.272-23.273s10.403-23.273 23.273-23.273c19.991 0 27.113-4.98 27.927-6.33.35-1.396-1.163-9.96-15.243-24.157L565.064 71.308c-28.323-28.486-78.243-28.463-106.566 0L61.558 470.342c-14.103 14.196-15.592 22.76-15.197 24.25.745 1.233 7.867 6.214 27.858 6.214l70.982-.838c39.773 0 75.752 33.745 75.752 70.819v351.302c0 14.452 27.392 43.334 38.168 43.334h71.726c13.335 0 29.743-14.802 29.743-26.857V756.154c0-35.142 36.072-93.277 77.172-93.277h164.399c44.171 0 84.224 79.011 84.224 109.754v159.465c0 14.336 14.01 33.327 24.552 33.327h60.114c13.335 0 31.697-16.478 31.697-28.416V569.809c0-12.846 10.403-23.272 23.273-23.272s23.273 10.426 23.273 23.272v367.22c.023 37.842-38.726 74.962-78.22 74.962z"
+            fill="#666
+"/>
+        </symbol>
+
+        <symbol id="msiteActive" viewBox="0 0 1030 1024">
+          <defs>
+          </defs>
+          <path
+            d="M771.074 1011.991h-60.113c-40.053 0-71.099-42.938-71.099-79.872V772.655c0-16.384-26.577-59.602-38.935-63.349l-163.165.14c-8.61.721-30.627 29.65-30.627 46.731V938.59c0 37.725-37.073 73.402-76.288 73.402h-71.726c-40.1 0-84.713-53.574-84.713-89.879V570.81c0-11.566-15.127-24.273-28.928-24.273l-70.982.838c-49.292 0-65.699-21.9-71.145-34.933-5.422-13.056-9.355-40.145 25.205-74.891l396.94-399.058C448.443 15.407 479.093 2.7 511.745 2.7h.023c32.675 0 63.325 12.707 86.296 35.793l396.94 399.034c34.56 34.77 30.626 61.86 25.204 74.915-5.423 13.056-21.853 34.933-70.89 34.933-12.869 0-23.272-10.427-23.272-23.273s10.403-23.273 23.273-23.273c19.991 0 27.113-4.98 27.927-6.33.35-1.396-1.163-9.96-15.243-24.157L565.064 71.308c-28.323-28.486-78.243-28.463-106.566 0L61.558 470.342c-14.103 14.196-15.592 22.76-15.197 24.25.745 1.233 7.867 6.214 27.858 6.214l70.982-.838c39.773 0 75.752 33.745 75.752 70.819v351.302c0 14.452 27.392 43.334 38.168 43.334h71.726c13.335 0 29.743-14.802 29.743-26.857V756.154c0-35.142 36.072-93.277 77.172-93.277h164.399c44.171 0 84.224 79.011 84.224 109.754v159.465c0 14.336 14.01 33.327 24.552 33.327h60.114c13.335 0 31.697-16.478 31.697-28.416V569.809c0-12.846 10.403-23.272 23.273-23.272s23.273 10.426 23.273 23.272v367.22c.023 37.842-38.726 74.962-78.22 74.962z"
+            fill="#3190e8"/>
+        </symbol>
+
+
+        <symbol xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 40 40" id="find">
+          <defs>
+            <path id="discover-regular.8ef537f_a"
+                  d="M20 40c11.046 0 20-8.954 20-20S31.046 0 20 0 0 8.954 0 20s8.954 20 20 20z"></path>
+            <mask id="discover-regular.8ef537f_b" width="40" height="40" x="0" y="0" fill="#fff">
+              <use xlink:href="#discover-regular.8ef537f_a"></use>
+            </mask>
+          </defs>
+          <g fill="none" fill-rule="evenodd">
+            <use stroke="#666" stroke-width="4" mask="url(#discover-regular.8ef537f_b)"
+                 xlink:href="#discover-regular.8ef537f_a"></use>
+            <path stroke="#666" stroke-width="2"
+                  d="M12.79 28.126c-1.515.68-2.169.016-1.462-1.484l3.905-8.284c.47-.999 1.665-2.198 2.66-2.675l8.484-4.064c1.497-.717 2.153-.08 1.46 1.435l-3.953 8.64c-.46 1.006-1.647 2.186-2.655 2.64l-8.44 3.792z"></path>
+            <path fill="#666"
+                  d="M15.693 24.636c-.692.276-1.02-.06-.747-.746l2.21-4.946c.225-.505.721-.602 1.122-.202l2.563 2.563c.394.394.31.893-.203 1.122l-4.945 2.209z"></path>
+          </g>
+        </symbol>
+
+        <symbol xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 40 40" id="findActive">
+          <defs>
+            <path id="discover-regular.8ef537f_a"
+                  d="M20 40c11.046 0 20-8.954 20-20S31.046 0 20 0 0 8.954 0 20s8.954 20 20 20z"></path>
+            <mask id="discover-regular.8ef537f_b" width="40" height="40" x="0" y="0" fill="#fff">
+              <use xlink:href="#discover-regular.8ef537f_a"></use>
+            </mask>
+          </defs>
+          <g fill="none" fill-rule="evenodd">
+            <use stroke="#3190e8" stroke-width="4" mask="url(#discover-regular.8ef537f_b)"
+                 xlink:href="#discover-regular.8ef537f_a"></use>
+            <path stroke="#3190e8" stroke-width="2"
+                  d="M12.79 28.126c-1.515.68-2.169.016-1.462-1.484l3.905-8.284c.47-.999 1.665-2.198 2.66-2.675l8.484-4.064c1.497-.717 2.153-.08 1.46 1.435l-3.953 8.64c-.46 1.006-1.647 2.186-2.655 2.64l-8.44 3.792z"></path>
+            <path fill="#3190e8"
+                  d="M15.693 24.636c-.692.276-1.02-.06-.747-.746l2.21-4.946c.225-.505.721-.602 1.122-.202l2.563 2.563c.394.394.31.893-.203 1.122l-4.945 2.209z"></path>
+          </g>
+        </symbol>
+
+        <symbol xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 38 38" id="order">
+          <defs>
+            <rect id="order-regular.41c17f8_a" width="38" height="38" rx="2"></rect>
+            <mask id="order-regular.41c17f8_b" width="38" height="38" x="0" y="0" fill="#fff">
+              <use xlink:href="#order-regular.41c17f8_a"></use>
+            </mask>
+          </defs>
+          <g fill="none" fill-rule="evenodd">
+            <use stroke="#666" stroke-width="4" mask="url(#order-regular.41c17f8_b)"
+                 xlink:href="#order-regular.41c17f8_a"></use>
+            <rect width="24" height="2" x="7" y="8" fill="#666" rx="1"></rect>
+            <rect width="20" height="2" x="7" y="17" fill="#666" rx="1"></rect>
+            <rect width="8" height="2" x="7" y="26" fill="#666" rx="1"></rect>
+          </g>
+        </symbol>
+
+        <symbol viewBox="0 0 38 38" id="orderActive">
+          <defs>
+            <linearGradient id="order.070ae2a_a" x1="50%" x2="50%" y1="100%" y2="0%">
+              <stop offset="0%" stop-color="#2BAEFF"></stop>
+              <stop offset="100%" stop-color="#0095FF"></stop>
+            </linearGradient>
+          </defs>
+          <g fill="none" fill-rule="evenodd">
+            <rect width="38" height="38" fill="url(#order.070ae2a_a)" rx="2"></rect>
+            <rect width="24" height="2" x="7" y="8" fill="#FFF" rx="1"></rect>
+            <rect width="20" height="2" x="7" y="17" fill="#FFF" rx="1"></rect>
+            <rect width="8" height="2" x="7" y="26" fill="#FFF" rx="1"></rect>
+          </g>
+        </symbol>
+
+        <symbol xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 38 38" id="profile">
+          <defs>
+            <path id="profile-regular.c151d62_a"
+                  d="M10 11.833V8.999A8.999 8.999 0 0 1 19 0c4.97 0 9 4.04 9 8.999v2.834l-.013.191C27.657 16.981 23.367 21 19 21c-4.616 0-8.64-4.02-8.987-8.976L10 11.833z"></path>
+            <mask id="profile-regular.c151d62_c" width="18" height="21" x="0" y="0" fill="#fff">
+              <use xlink:href="#profile-regular.c151d62_a"></use>
+            </mask>
+            <path id="profile-regular.c151d62_b"
+                  d="M0 32.675C0 26.763 10.139 22 19.027 22 27.916 22 38 26.763 38 32.757v3.312C38 37.136 37.098 38 35.997 38H2.003C.897 38 0 37.137 0 36.037v-3.362z"></path>
+            <mask id="profile-regular.c151d62_d" width="38" height="16" x="0" y="0" fill="#fff">
+              <use xlink:href="#profile-regular.c151d62_b"></use>
+            </mask>
+          </defs>
+          <g fill="none" fill-rule="evenodd" stroke="#666" stroke-width="4">
+            <use mask="url(#profile-regular.c151d62_c)" xlink:href="#profile-regular.c151d62_a"></use>
+            <use mask="url(#profile-regular.c151d62_d)" xlink:href="#profile-regular.c151d62_b"></use>
+          </g>
+        </symbol>
+
+        <symbol viewBox="0 0 38 38" id="profileActive">
+          <defs>
+            <linearGradient id="profile.dbc5ebf_a" x1="50%" x2="50%" y1="100%" y2="0%">
+              <stop offset="0%" stop-color="#2BAEFF"></stop>
+              <stop offset="100%" stop-color="#0095FF"></stop>
+            </linearGradient>
+          </defs>
+          <path fill="url(#profile.dbc5ebf_a)" fill-rule="evenodd"
+                d="M10 11.833V8.999A8.999 8.999 0 0 1 19 0c4.97 0 9 4.04 9 8.999v2.834l-.013.191C27.657 16.981 23.367 21 19 21c-4.616 0-8.64-4.02-8.987-8.976L10 11.833zM0 32.675C0 26.763 10.139 22 19.027 22 27.916 22 38 26.763 38 32.757v3.312C38 37.136 37.098 38 35.997 38H2.003C.897 38 0 37.137 0 36.037v-3.362z"></path>
+        </symbol>
+
+      </defs>
+    </svg>
+    <section @click="gotoAddress({path: '/msite', query: {geohash}})" class="guide_item">
+      <svg class="icon_style">
+        <use xmlns:xlink="http://www.w3.org/1999/xlink"
+             :xlink:href="$route.path.indexOf('msite') !== -1? '#msiteActive' : '#msite'"></use>
+      </svg>
+      <span>首页</span>
+    </section>
+
+    <section @click="gotoAddress({path: '/area'})" class="guide_item">
+      <svg class="icon_style">
+        <use xmlns:xlink="http://www.w3.org/1999/xlink"
+             :xlink:href="$route.path.indexOf('area') !== -1? '#findActive' : '#find'"></use>
+      </svg>
+      <span>区域</span>
+    </section>
+
+    <section @click="gotoAddress({path: `/news`})" class="guide_item">
+      <svg class="icon_style">
+        <use xmlns:xlink="http://www.w3.org/1999/xlink"
+             :xlink:href="$route.path.indexOf('news') !== -1? '#findActive' : '#find'"></use>
+      </svg>
+      <span>资讯</span>
+    </section>
+    <section @click="gotoAddress('/order')" class="guide_item">
+      <svg class="icon_style">
+        <use xmlns:xlink="http://www.w3.org/1999/xlink"
+             :xlink:href="$route.path.indexOf('order') !== -1? '#orderActive' : '#order'"></use>
+      </svg>
+      <span>订单</span>
+    </section>
+    <section @click="gotoAddress('/profile')" class="guide_item">
+      <svg class="icon_style">
+        <use xmlns:xlink="http://www.w3.org/1999/xlink"
+             :xlink:href="$route.path.indexOf('profile') !== -1? '#profileActive' : '#profile'"></use>
+      </svg>
+      <span>我的</span>
+    </section>
+  </section>
+</template>
+
+<script>
+  import {mapState} from 'vuex'
+
+  export default {
+    data() {
+      return {}
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    computed: {
+      ...mapState([
+        'geohash'
+      ]),
+    },
+    methods: {
+      gotoAddress(path) {
+        this.$router.push(path)
+      }
+    },
+
+  }
+
+</script>
+
+<style lang="scss" scoped>
+  @import '../../style/mixin';
+
+  #foot_guide {
+    background-color: #fff;
+    position: fixed;
+    z-index: 100;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    @include wh(100%, 1.95rem);
+    display: flex;
+    box-shadow: 0 -0.026667rem 0.053333rem rgba(0, 0, 0, .1);
+  }
+
+  .guide_item {
+    flex: 1;
+    display: flex;
+    text-align: center;
+    flex-direction: column;
+    align-items: center;
+    .icon_style {
+      @include wh(.8rem, .8rem);
+      margin-top: .3rem;
+      fill: #ccc;
+    }
+    span {
+      @include sc(.45rem, #666);
+    }
+  }
+
+</style>
